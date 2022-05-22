58610798bd736421d0d4e507bc5bedfa2559aa3a
AnhNH01 <haianh552@gmail.com>
2022-05-22 16:25:06 +0700

    Change text alignment, clean up code

--- app/src/main/java/com/example/droidcafe/OrderActivity.java
+++ app/src/main/java/com/example/droidcafe/OrderActivity.java
@@ -1,7 +1,5 @@
 package com.example.droidcafe;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +10,8 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class OrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -27,39 +27,36 @@ public class OrderActivity extends AppCompatActivity implements AdapterView.OnIt
         textView.setText(message);
 
         radioGroup = (RadioGroup) findViewById(R.id.delivery_method);
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                RadioButton radioButton = radioGroup.findViewById(i);
-                switch (radioButton.getId()){
-                    case R.id.sameday:
-                        displayToast(getString(R.string.same_day_messenger_service));
-                        break;
-                    case R.id.nextday:
-                        displayToast(getString(R.string.next_day_ground_delivery));
-                        break;
-                    case R.id.pickup:
-                        displayToast(getString(R.string.pick_up));
-                        break;
-                    default:
-                        break;
-                }
-
+        radioGroup.setOnCheckedChangeListener((radioGroup, i) -> {
+            RadioButton radioButton = radioGroup.findViewById(i);
+            switch (radioButton.getId()) {
+                case R.id.sameday:
+                    displayToast(getString(R.string.same_day_messenger_service));
+                    break;
+                case R.id.nextday:
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                    break;
+                case R.id.pickup:
+                    displayToast(getString(R.string.pick_up));
+                    break;
+                default:
+                    break;
             }
+
         });
         // Create the spinner
         Spinner spinner = findViewById(R.id.label_spinner);
-        if(spinner != null)
+        if (spinner != null)
             spinner.setOnItemSelectedListener(this);
 
         // Create ArrayAdapter using the string array and default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.labels_array,
-                                                                                        android.R.layout.simple_spinner_item);
+                android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         // Apply the adapter to spinner
-        if(spinner != null)
+        if (spinner != null)
             spinner.setAdapter(adapter);
     } // End of OnCreate()
 
